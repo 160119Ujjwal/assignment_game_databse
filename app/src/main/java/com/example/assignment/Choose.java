e09fe267139eabe18df2af32f3d5846117ca6a75
ujjwal <ujjwalkhaitu@gmail.com>
2019-12-18 08:31:02 +0545

    Initial commit

---
+++ app/src/main/java/com/example/assignment/Choose.java
@@ -0,0 +1,39 @@
+package com.example.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.assignment.R;
+
+public class Choose extends AppCompatActivity {
+
+    Button word,student;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+        word= findViewById(R.id.word);
+        student= findViewById(R.id.std);
+
+        word.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(Choose.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Choose.this,Database.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
